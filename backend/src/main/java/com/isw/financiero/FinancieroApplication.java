e93a441d8dbae7941b4982da9f084774d934612d
Jhonatan <jhonatanlaragomez@gmail.com>
2026-05-24 20:43:00 -0500

    JL-ISW1JLDS-pruebas: Fix rutas y restaurar archivos eliminados

---
+++ backend/src/main/java/com/isw/financiero/FinancieroApplication.java
@@ -0,0 +1,12 @@
+package com.isw.financiero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancieroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancieroApplication.class, args);
+    }
+}
